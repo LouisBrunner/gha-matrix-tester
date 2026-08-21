a0adc9593cd50969208c476f9cf8bd222fca1e86
Louis Brunner <code@e.lbrunner.net>
2026-08-21 21:48:25 +0100

    fix: CSP

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,29 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import csp from "vite-plugin-csp-guard";
+import sri from "vite-plugin-sri-gen";
 
 export default defineConfig(() => ({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		csp({
+			algorithm: "sha256",
+			override: true,
+			policy: {
+				"base-uri": ["'none'"],
+				"connect-src": ["'self'"],
+				"default-src": ["'none'"],
+				"form-action": ["'none'"],
+				"img-src": ["'self'"],
+				"object-src": ["'none'"],
+				"script-src": ["'self'", "https://static.cloudflareinsights.com"],
+				"style-src": ["'self'", "'unsafe-inline'"],
+			},
+		}),
+		sri(),
+	],
 	resolve: {
 		tsconfigPaths: true,
 	},
